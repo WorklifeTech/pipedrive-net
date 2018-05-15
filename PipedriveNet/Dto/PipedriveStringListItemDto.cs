d245f66880dbaa78bc59c0224883fcf0abc56192
Mads Dorup <mads@dorup.dk>
2018-05-15 11:12:33 +0200

    Formatting

--- PipedriveNet/Dto/PipedriveStringListItemDto.cs
+++ PipedriveNet/Dto/PipedriveStringListItemDto.cs
@@ -3,7 +3,9 @@
     public class PipedriveStringListItemDto
     {
         public bool Primary { get; set; }
+
         public string Value { get; set; }
+
         public override string ToString()
         {
             return Value;
@@ -11,7 +13,7 @@
 
         public static implicit operator string(PipedriveStringListItemDto item)
         {
-            return item == null ? null : item.Value;
+            return item?.Value;
         }
     }
 }
